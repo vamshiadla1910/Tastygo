38e48b0510e516a50a7f2d0b130a5c7618ff0aa7
Bhargavi <bhargavigavini85@gmail.com>
2026-06-30 15:56:57 +0530

    My local changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Loginpage from "./Components/Login"
+import Navbar from "./Components/Navbar"
+import "./App.css"
+import Hero from "./Components/Hero"
+function App() {
+  return (
+  <>
+  <Navbar/>
+  <Hero/>
+  <Loginpage/>
+  </>
+  )
+}
+
+export default App
